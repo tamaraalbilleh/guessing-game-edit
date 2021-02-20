d278e8e51aa18d524bf3733b96dbd5ee7dcbd2f6
tamaraalbilleh <tamara2albella@gmail.com>
2021-02-20 15:51:41 +0200

    synced with the non-edit one

--- js/app.js
+++ js/app.js
@@ -1,209 +1,175 @@
 'use strict';
-
-
 let score =0;
 
-
+// welcome message //
 let userName = prompt ('Hi there, whats your name ?' );
-//console.log(userName);
+function nameConfirmation (a){
+  let empty = '';
+  userName = a;
+  while (userName === empty ){
+    userName = prompt ('please enter your name ,I want us to be friends ');
+  }
+  return (a);
+}
+
+nameConfirmation (userName);
 alert ('Nice to meet you '+ userName + ' my name is Tamara');
-alert (' lets see how much can you guess about me ^_^ , please answer only with a (y) or (n) ');
+alert (' lets see how much you can guess about me ^_^ , please answer with a yes/y or no/n ');
 
-
-//first question//
-
-const firstQuestion = function (){
-
-  let numOfFriends = prompt ('lets start with the first question , do i have more than one best friend ? y/n ');
-  numOfFriends = numOfFriends.toLowerCase();
-  switch (numOfFriends){
+//answer validity check//
+function questionsYes (userAnswer,d,e,f,g,h){
+  userAnswer.toLowerCase();
+  while ((userAnswer.toLowerCase() !== 'yes') && (userAnswer.toLowerCase() !== 'y') && (userAnswer.toLowerCase() !== 'no') && (userAnswer.toLowerCase() !== 'n')){
+    console.log ('Invalid answer, please try again with a yes/no or y/n');
+    alert ('invalid answer , please try again with a yes/no or a y/n');
+    userAnswer =prompt (d.toLowerCase());
+  }
+  switch (userAnswer.toLowerCase()){
   case 'y':
   case 'yes':
-  //console.log('1st q answer is correct');
-    alert ('Yes , that is correct , thier names are Roaya and Misaa' + ' good job '+ userName );
+    console.log (e);
+    alert (f);
     score++;
     alert (' your current score is '+ score +' out of 7');
     break;
   case 'n':
   case 'no':
-  //console.log('1st q answer is incorrect');
-    alert ('I am sorry , it is the wrong answer , it is 2021 you can have more than one best friend XD , I have 2');
+    console.log (g);
+    alert (h);
     alert (' your current score is '+ score +' out of 7');
     break;
   }
-};
+  return (userAnswer,d,e,f,g,h);
+}
 
-firstQuestion();
+function questionsNo (userAnswer,d,e,f,g,h){
+  userAnswer.toLowerCase();
+  while ((userAnswer.toLowerCase() !== 'yes') && (userAnswer.toLowerCase() !== 'y') && (userAnswer.toLowerCase() !== 'no') && (userAnswer.toLowerCase() !== 'n')){
+    console.log ('Invalid answer, please try again with a yes/no or y/n');
+    alert ('invalid answer , please try again with a yes/no or a y/n');
+    userAnswer =prompt (d.toLowerCase());
+  }
+  switch (userAnswer.toLowerCase()){
+  case 'y':
+  case 'yes':
+    console.log (g);
+    alert (h);
+    alert (' your current score is '+ score +' out of 7');
+    break;
+  case 'n':
+  case 'no':
+    console.log (e);
+    alert (f);
+    score++;
+    alert (' your current score is '+ score +' out of 7');
+    break;
+  }
+  return (userAnswer,d,e,f,g,h);
+}
+
+
+// first question //
+
+let numOfFriends = prompt ('lets start with the first question , do i have more than one best friend ? ');
+numOfFriends = numOfFriends.toLowerCase();
+let wrongMessage = ' do i have more than one best friend ? ';
+let consoleMessageRight = '1st q answer is correct';
+let consoleMessageWrong = '1st q answer is incorrect';
+let userMessageRight = 'Yes , that is correct , thier names are Roaya and Misaa' + ' good job '+ userName ;
+let userMessageWrong = 'I am sorry , it is the wrong answer , it is 2021 you can have more than one best friend XD , I have 2';
+questionsYes (numOfFriends,wrongMessage,consoleMessageRight,userMessageRight,consoleMessageWrong,userMessageWrong);
 
 // second question //
-
-
-function secondQuestion (){
-  let animeFan = prompt ('so '+userName+' do you think I like to watch anime ? y/n');
-  animeFan = animeFan.toLowerCase();
-  switch (animeFan){
-  case 'y':
-  case 'yes':
-  //console.log('2nd q answer is correct');
-    alert ('Yes good job , i do love anime , i watch it all the time , you should try watching it too '+ userName);
-    score++ ;
-    alert (' your current score is '+ score +' out of 7');
-    break;
-  case 'n':
-  case 'no':
-  //console.log('2nd q answer is incorrect');
-    alert ('Noooo , i love anime T-T');
-    alert (' your current score is '+ score +' out of 7');
-    break;
-  }
-}
-
-secondQuestion();
-
-
-
+let animeFan = prompt ('so '+userName+' do you think I like to watch anime ?');
+animeFan = animeFan.toLowerCase();
+wrongMessage = 'do you think I like to watch anime ?';
+consoleMessageRight = '2nd q answer is correct';
+consoleMessageWrong = '2nd q answer is incorrect';
+userMessageRight = ('Yes good job , i do love anime , i watch it all the time , you should try watching it too '+ userName) ;
+userMessageWrong = 'Noooo , i love anime T-T';
+questionsYes (animeFan,wrongMessage,consoleMessageRight,userMessageRight,consoleMessageWrong,userMessageWrong);
 
 // third question //
-
-
-const thirdQuestion = function (){
-  let gameFan = prompt ('now since we are talking about geeky stuff , do you think i like video games? y/n');
-  gameFan = gameFan.toLowerCase();
-  switch (gameFan){
-  case 'y':
-  case 'yes':
-  //console.log('3rd q answer is correct');
-    alert ('Yeees I am a gamer ');
-    score++ ;
-    alert (' your current score is '+ score +' out of 7');
-    break;
-  case 'n':
-  case 'no':
-  //console.log('3rd q answer is incorrect');
-    alert ('no ,that was an easy one , most anime fans are also gamers T-T');
-    alert (' your current score is '+ score +' out of 7');
-    break;
-  }
-};
-
-thirdQuestion();
-
-
+let gameFan = prompt ('now since we are talking about geeky stuff , do you think i like video games? y/n');
+gameFan = gameFan.toLowerCase();
+wrongMessage = 'do you think I like video games ?';
+consoleMessageRight = '3rd q answer is correct';
+consoleMessageWrong = '3rd q answer is incorrect';
+userMessageRight = ('Yeees I am a gamer ') ;
+userMessageWrong = 'no ,that was an easy one , most anime fans are also gamers T-T';
+questionsYes (gameFan,wrongMessage,consoleMessageRight,userMessageRight,consoleMessageWrong,userMessageWrong);
 
 // fourth question //
+let artFan = prompt ('How about drawing , do I like to draw ? y/n');
+artFan = artFan.toLowerCase();
+wrongMessage = 'do you think I like to draw ?';
+consoleMessageRight = '4th q answer is correct';
+consoleMessageWrong = '4th q answer is incorrect';
+userMessageRight = ('yeah , I draw a little digitally , I am not very good at it though XD') ;
+userMessageWrong = ('incorrect , I actually draw a little in my free time, sorry '+ userName);
+questionsYes (artFan,wrongMessage,consoleMessageRight,userMessageRight,consoleMessageWrong,userMessageWrong);
+
+// // fifth question //
+let birthDate = prompt ('here is a tricky question , there is an anime character named Tama the cat ,we share the same initials but `not` the same birth date ? y/n');
+birthDate= birthDate.toLowerCase();
+wrongMessage = 'there is an anime character named Tama the cat ,we share the same initials but `not` the same birth date ? y/n';
+consoleMessageRight = '5th q answer is correct';
+consoleMessageWrong = '5th q answer is incorrect';
+userMessageRight = ('yup , and that is why i call myself Tama , there is a picture for this cat in the website ^_^ ') ;
+userMessageWrong = ('sorry '+ userName+ ' we do share the same initials and birth date which is 28th of june');
+questionsNo (birthDate,wrongMessage,consoleMessageRight,userMessageRight,consoleMessageWrong,userMessageWrong);
 
 
-function fourthQuestion () {
-  let artFan = prompt ('How about drawing , do I like to draw ? y/n');
-  artFan = artFan.toLowerCase();
-  switch (artFan){
-  case 'y':
-  case 'yes':
-  //console.log('4th q answer is correct');
-    alert ('yeah , I draw a little digitally , I am not very good at it though XD');
-    score++ ;
-    alert (' your current score is '+ score +' out of 7');
-    break;
-  case 'n':
-  case 'no':
-  //console.log('4th q answer is incorrect');
-    alert ('incorrect , I actually draw a little in my free time, sorry '+ userName);
-    alert (' your current score is '+ score +' out of 7');
-    break;
-  }
+// //sixth question //
+alert ('now try to guess my favorite number , you have 4 attempts , here is a hint , its between 0 and 10 ');
+let favNum = prompt ('Now , what is my favorite number? ');
+let trials = 4;
+// to extract a number from user answer //
+while (favNum !== parseInt(favNum, 10).toString()){
+  alert ('invalid answer , please enter a number');
+  favNum = prompt ('what is my favorite number ?');
 }
-
-fourthQuestion();
-
-// fifth question //
-
-
-const fifthQuestion = function () {
-  let birthDate = prompt ('here is a tricky question , there is an anime character named Tama the cat ,we share the same initials but `not` the same birth date ? y/n');
-  birthDate= birthDate.toLowerCase();
-  switch (birthDate){
-  case 'y':
-  case 'yes':
-  //console.log('5th q answer is incorrect');
-    alert ('sorry '+ userName+ ' we do share the same initials and birth date which is 28th of june');
-    alert (' your current score is '+ score +' out of 7');
-    break;
-  case 'n':
-  case 'no':
-  //console.log('5th q answer is correct');
-    alert ('yup , and that is why i call myself Tama , there is a picture for this cat in the website ^_^ ');
+while (trials){
+  if (favNum === '6'){
+    alert (' correct answer ');
     score++ ;
-    alert (' your current score is '+ score +' out of 7');
-    break;
-  }
-};
-
-fifthQuestion();
-
-
-
-
-//sixth question //
-
-function sixthQuestion () {
-  alert ('now try to guess my favorite number , you have 4 attempts , here is a hint , its between 0 and 10 ');
-  let favNum = prompt ('Now , what is my favorite number? ');
-  let trials = 3;
-  while (trials){
-    if (favNum === '6'){
-      alert (' correct answer ');
-      score++ ;
-      trials = 0;
-    }else if (favNum !== 6){
-      if (favNum < 6){
-        alert ('too low , try again , maybe higher ');
-        favNum = prompt ('what is my favorite number? ');
-        trials--;
-        score;
-      }else if (favNum > 6){
-        alert ('too high , try lower ');
-        favNum = prompt ('what is my favorite number? ');
-        trials--;
-        score;
-      }
+    trials = 0;
+  }else if (favNum !== 6){
+    if (favNum < 6){
+      alert ('too low , try again , maybe higher ');
+      favNum = prompt ('what is my favorite number? ');
+      trials = trials-1;
+      score;
+    }else if (favNum > 6){
+      alert ('too high , try lower ');
+      favNum = prompt ('what is my favorite number? ');
+      trials = trials-1;
+      score;
     }
   }
-  alert (' my favorite number is the number 6 because its my birthday month');
-  alert (' your current score is '+ score +' out of 7');
 }
-
-sixthQuestion();
-
-
-
+alert (' my favorite number is the number 6 because its my birthday month');
+alert (' your current score is '+ score +' out of 7');
 
 //seventh question //
-
-
-
-const seventhQuestion = function () {
-  let answer = ['zaid', 'saif'];
-  let brothers = prompt ('I have two brothers , can you guess one of their names? hint : choose from these names(mohammad , ahmad, zaid, abdallah,saif,omar) you have 6 attempts');
-  brothers.toLowerCase();
-  let tryBrothers = 5;
-  while (tryBrothers){
-    if (brothers === answer [0] || brothers === answer [1] ){
-      alert ('correct answer , good job '+ userName);
-      tryBrothers = 0;
-      score++;
-    }else if (brothers !== answer) {
-      alert ('wrong answer');
-      brothers = prompt ('try again, guess one of their names? hint : choose from these names(mohammad , ahmad, zaid, abdallah,saif,omar)');
-      tryBrothers--;
-    }
+let answer = ['zaid', 'saif'];
+let brothers = prompt ('I have two brothers , can you guess one of their names? hint : choose from these names(mohammad , ahmad, zaid, abdallah,saif,omar) you have 6 attempts');
+brothers.toLowerCase();
+let tryBrothers = 5;
+while (tryBrothers){
+  if ((brothers.toLowerCase () === answer [0] )||( brothers.toLowerCase () === answer [1] )){
+    alert ('correct answer , good job '+ userName);
+    tryBrothers = 0;
+    score++;
+  }else if (brothers.toLowerCase () !== answer[0,1]) {
+    alert ('wrong answer');
+    brothers = prompt ('try again, guess one of their names? hint : choose from these names(mohammad , ahmad, zaid, abdallah,saif,omar)');
+    tryBrothers--;
   }
+}
 
 
-  alert (' my two brothers are Zaid and Saif ,');
-
-};
-
-seventhQuestion();
+alert (' my two brothers are Zaid and Saif ,');
 
 
 
@@ -217,7 +183,6 @@ if (score < 4){
   alert ('great , you WON , you did a great job '+ userName);
 }
 alert (' Thank you for playing along '+ userName +' This was fun ^_^');
-
 
 
 
